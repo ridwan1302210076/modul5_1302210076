8f92da71ec389fac74995541b8c69210c333fdcb
ridwan1302210076 <127034652+ridwan1302210076@users.noreply.github.com>
2023-03-24 08:20:44 +0700

    commit pertama

--- modul5_1302210076/Program.cs
+++ modul5_1302210076/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Collections.Specialized;
+
+internal class program
+{
+    private static void Main(string[] args)
+
+    {
+        Penjumlahan<int> pen = new Penjumlahan<int>();
+        pen.JumlahTigaAngka(13, 02, 21);
+    }
+}
+
+public class Penjumlahan<T>
+{ 
+    public void JumlahTigaAngka(T a, T b, T c)
+
+    {
+        dynamic aa = a;
+        dynamic bb = b;
+        dynamic cc = c; 
+        Console.WriteLine(aa+bb+cc);
+    }
+}
